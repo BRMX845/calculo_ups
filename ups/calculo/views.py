0aba1d1291a7bf18ed6880f67045a418b71046d8
bmrx845 <bquelali8@gmail.com>
2023-05-12 20:36:24 -0400

    final
    aprobado por ingeniero

--- ups/calculo/views.py
+++ ups/calculo/views.py
@@ -31,12 +31,25 @@ def calcular(request):
         cant_puertas = int(request.POST.get('puertas'))
         puertas=cant_puertas*150
 
+
         Fact = 0.8
         
         res = (serv+switches+ventiladores+racks+dmz+routers+modems+pbx+monitores+wifis+pcs+sensores+nas+puertas)* Fact
+        but=res*3.41
+        res_but=round(but,2)
         resultado=round(res,2)
         ups_eje=""
         url=""
+        enf=""
+        url_enf=""
+        if res_but <=7000:
+            enf="Aire Acondicionado Portátil KENDAL 7000 BTU Sólo Frío"
+            url_enf="https://portaldelaire.cl/products/aire-acondicionado-portatil-kendal-7000-btu-solo-frio"
+            res_but=7000
+        elif res_but >7000 and res_but<=9000:
+            url_enf="https://www.lg.com/cac/aire-acondicionado-residencial/lg-vm092c9"
+            enf="Aire Acondicionado LG DUALCOOL Inverter, 9000 BTU, Ahorro de Energía, Micro Filtro, Funcionamiento silencioso, 10 años de garantía en el compresor"
+            res_but=9000
         if resultado >=1000 and resultado<2000:
             ups_eje="puede usar un 220V - Winner Pro 2k"
             url="https://www.alibaba.com/product-detail/New-Product-2Kva-1-6Kw-2000Va_1600822274763.html?spm=a2700.7735675.0.0.21a7c7Ouc7OuPO&s=p"
@@ -44,6 +57,6 @@ def calcular(request):
             ups_eje="puede usar un 220V - Winner Pro 3k"
             url="https://www.alibaba.com/product-detail/New-Product-2Kva-1-6Kw-2000Va_1600822274763.html?spm=a2700.7735675.0.0.21a7c7Ouc7OuPO&s=p"
         
-        return render(request, 'calcular.html', {'resultado': resultado,'ejemplo':ups_eje,'compra':url})
+        return render(request, 'calcular.html', {'resultado': resultado,'ejemplo':ups_eje,'compra':url,'btus':res_but,'enfriamiento':enf,'url_enfr':url_enf})
 
     return render(request, 'calcular.html')
